ad63b51687397114d55d166752e4b52832eeeb22
Cisco <cisco@FIC.ORG.AR>
2019-06-24 20:21:16 -0300

    paralelismo

---
+++ paralelismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace paralelismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
