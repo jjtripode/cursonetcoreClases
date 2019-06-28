1089ef47fd0d1b5332661c41f7f860863b50b0e0
jjtripode@hotmail.com <cisco@FIC.ORG.AR>
2019-06-28 19:45:27 -0300

    segundo commit

--- Eventos/Numeros.cs
+++ Eventos/Numeros.cs
@@ -4,10 +4,22 @@ namespace Eventos
     {
         private int num;
 
-        public int Num { get => num; set => num = value; }
+        public int Num
+        {
+            get => num;
+            set
+            {
+                  num = value;
+                if (num % 2 == 0 && Evento_Par != null)
+                {
+                  Evento_Par();
+                }
+               
+            }
+        }
 
         public delegate void NumeroPar();
 
-       public event NumeroPar EventoPar;
-       }
+        public event NumeroPar Evento_Par;
+    }
 }
--- Eventos/Program.cs
+++ Eventos/Program.cs
@@ -6,7 +6,26 @@ namespace Eventos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Numeros num = new Numeros();
+
+            Console.WriteLine($"Comprobando si el numero {2} es par.");
+
+            num.Evento_Par += DeteccionNumeroPar;
+           
+            num.Evento_Par += ()=> Console.WriteLine($"Desde Lanmda");
+            num.Evento_Par += delegate() {Console.WriteLine($"Desde Delegado");};
+
+            // while (Console.ReadLine() != "F")
+            // {
+                num.Num = 2;
+            // }
+
+            Console.ReadKey();
+        }
+
+        private static void DeteccionNumeroPar()
+        {
+            Console.WriteLine("El numero es par");
         }
     }
 }
