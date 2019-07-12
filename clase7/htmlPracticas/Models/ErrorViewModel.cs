38df3567f4a31fc03d7bf996ad9648f0b2963a9a
jjtripode@hotmail.com <cisco@FIC.ORG.AR>
2019-07-12 20:55:06 -0300

    subo clase 7

---
+++ clase7/htmlPracticas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace htmlPracticas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
