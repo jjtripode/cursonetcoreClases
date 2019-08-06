d47b7e0fa331295ca665526faa71a16abf5d1ff7
jjtripode@hotmail.com <cisco@FIC.ORG.AR>
2019-08-05 21:05:59 -0300

    se agrego loggin

--- AspNetCoreTodo-UTN-master/AspNetCoreTodo/Controllers/HomeController.cs
+++ AspNetCoreTodo-UTN-master/AspNetCoreTodo/Controllers/HomeController.cs
@@ -5,11 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreTodo.Models;
+using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,13 +25,19 @@ namespace AspNetCoreTodo.Controllers
 
         public IActionResult Privacy()
         {
+            throw new Exception("Error en accion privacy");
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var exceptionHandlerPathFeature =
+                HttpContext.Features.Get<Microsoft.AspNetCore.Diagnostics.IExceptionHandlerPathFeature>();
+
+                logger.LogError($" Date: {DateTime.Now}  error al invocar: {exceptionHandlerPathFeature.Path} - {exceptionHandlerPathFeature.Error}");
+
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier, Path = exceptionHandlerPathFeature.Path });
         }
     }
 }
